0784cc55ff6bcc3712373cb78ccdbd1a0ed42a91
Joy Zhang <joy.xczhang@gmail.com>
2016-06-18 16:05:03 +0800

    update feeder

--- scripts/feeder.rb
+++ scripts/feeder.rb
@@ -1,6 +1,6 @@
 require 'json'
 
-books = JSON.parse(File.open("./books.json").read)
+books = JSON.parse(File.open("scripts/books.json").read)
 
 books.each do |book|
     puts "insert into bookish.books(title, author, published_at, isbn) values('#{book["title"]}', '#{book["author"].join(',')}', '#{book["published_at"]}', '#{book["isbn"].last}');"
